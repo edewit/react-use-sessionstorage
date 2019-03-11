7e33b00646709037d4356552a461f4a9a3dc6e11
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-03-11 16:38:54 +0100

    fix: use parse and stringify to store objects
    action can be a function thanks to @ia3andy

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
-import React, { Dispatch } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
-export default function useSessionStorage(
+export function useSessionStorage(
   key: string,
   initialValue: string = ''
-): [string, Dispatch<string>] {
+): [string, Dispatch<SetStateAction<string>>, () => void] {
   const [item, setValue] = React.useState(() => {
     const value = sessionStorage.getItem(key) || initialValue;
     sessionStorage.setItem(key, value);
     return value;
   });
 
-  const setItem = (newValue: string) => {
-    setValue(newValue);
-    sessionStorage.setItem(key, newValue);
+  const setItem = (action: SetStateAction<string>) => {
+    if(action instanceof Function) {
+      return setValue((prevState) => {
+        const value = action(prevState);
+        sessionStorage.setItem(key, value);
+        return value;
+      });
+    }
+    setValue(action);
+    sessionStorage.setItem(key, action);
   };
 
-  return [item, setItem];
+  const clear = () => {
+    sessionStorage.removeItem(key);
+  };
+
+  return [item, setItem, clear];
+}
+
+export function useSessionStorageWithObject<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>, () => void] {
+  const [_item, _setItem, clear] = useSessionStorage('app', JSON.stringify(initialValue));
+  const item: T = JSON.parse(_item);
+  const setApp = (value: SetStateAction<T>) => {
+    if(value instanceof Function) {
+      _setItem((prevState) => JSON.stringify(value(JSON.parse(prevState))));
+      return;
+    }
+    _setItem(JSON.stringify(value));
+  };
+  return [item, setApp, clear];
 }
